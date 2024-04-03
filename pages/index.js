e167e3c8f41e1e37b7d7146c59596b252de431ae
YasserMOUSTAINE <moustaineyasser7272@gmail.com>
2024-04-03 07:22:33 +0000

    78

--- pages/index.js
+++ pages/index.js
@@ -128,8 +128,8 @@ export default function Home() {
             </div>
           </div>
           <div className=" col-span-1 row-span-1 pt-8">
-            <div className=" bg-white w-[199px] h-[44px] rounded-xl flex  mt-3 border-[#1AD079] border-2">
-              <button type="submit" className="  w-[159px] h-[36px] m-0.5 rounded-[10px] bg-[#1AD079] text-white transition-width duration-1000 hover:w-[165px]">Candidater</button>
+            <div className=" bg-white w-[199px] h-[44px] rounded-xl flex  mt-3 border-[#0073F5] border-2">
+              <button type="submit" className="  w-[159px] h-[36px] m-0.5 rounded-[10px] bg-[#0073F5] text-white transition-width duration-1000 hover:w-[165px]">Candidater</button>
                 <div className="flex items-center pl-3 " >
                   <svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" fill="none" viewBox="0 0 8 13" className="grid content-center">
                     <path fill="#0073F5" d="m1.226 10.506 3.972-3.972-3.972-3.972A1.02 1.02 0 1 1 2.669 1.12l4.699 4.699a1.02 1.02 0 0 1 0 1.443L2.669 11.96c-.399.4-1.044.4-1.443 0a1.039 1.039 0 0 1 0-1.453Z"/>
