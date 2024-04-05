045141641f0bf2ac77ce6591f2fe8d7fec4028c6
YasserMOUSTAINE <moustaineyasser7272@gmail.com>
2024-04-05 23:24:04 +0000

    gha had lmeraa

--- pages/accordian/Accordian.js
+++ pages/accordian/Accordian.js
@@ -1,27 +1,10 @@
 import AccordianList from "./AccordianList";
-//import Data from "./Data";
+import Data from "./Data";
 import { useState } from "react";
 
 const Accordian = () => {
     
-    const Data=[
-        {
-            id:1,
-            question:"amet, consectetur adipiscing elit, sed do eiusmod te",
-            reponse: "The Institute is a top institution in the Middle East and North Africa forto international students and professionals. It offers tailored courses and cultural immersion programs, introducing participants to and Moroccan culture."
-        },
-        {
-            id:2,
-            question:"amet, consectetur adipiscing elit, sed do eiusmod te",
-            reponse: "The Institute is a top institution in the Middle East and North Africa forto international students and professionals. It offers tailored courses and cultural immersion programs, introducing participants to and Moroccan culture."
-        },
-        {
-            id:3,
-            question:"amet, consectetur adipiscing elit, sed do eiusmod te",
-            reponse: "The Institute is a top institution in the Middle East and North Africa forto international students and professionals. It offers tailored courses and cultural immersion programs, introducing participants to and Moroccan culture."
-        }
-    ]
-
+    
     const [touch,setTouch]=useState(null);
     let handleTouch=(id)=>{
         if(touch===id){
