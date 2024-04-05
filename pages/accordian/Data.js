3734c8ee670814fc474d2627128ee9d753e70ba7
YasserMOUSTAINE <moustaineyasser7272@gmail.com>
2024-04-05 22:49:33 +0000

    h

--- pages/accordian/Data.js
+++ pages/accordian/Data.js
@@ -1,4 +1,4 @@
-const Data=[
+export default Data=[
     {
         id:1,
         question:"amet, consectetur adipiscing elit, sed do eiusmod te",
@@ -15,4 +15,3 @@ const Data=[
         reponse: "The Institute is a top institution in the Middle East and North Africa forto international students and professionals. It offers tailored courses and cultural immersion programs, introducing participants to and Moroccan culture."
     }
 ]
-export default Data;
